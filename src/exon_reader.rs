10eca773d3fa5e04224ffb5e33b72bb828da893d
Trent Hauck <trent@trenthauck.com>
2023-09-13 08:04:48 -0700

    feat: add query_vcf_file method

--- src/exon_reader.rs
+++ src/exon_reader.rs
@@ -18,7 +18,7 @@ use std::sync::Arc;
 
 use arrow::ffi_stream::{ArrowArrayStreamReader, FFI_ArrowArrayStream};
 use arrow::pyarrow::IntoPyArrow;
-use datafusion::datasource::file_format::file_type::FileCompressionType;
+use datafusion::common::FileCompressionType;
 use datafusion::prelude::{SessionConfig, SessionContext};
 use exon::datasources::ExonFileType;
 use exon::ffi::create_dataset_stream_from_table_provider;
