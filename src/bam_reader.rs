51ba9923157fa2872aa2185e9a7a39ec83fe4248
Trent Hauck <trent@trenthauck.com>
2023-10-05 23:42:23 -0700

    build: bump exon

--- src/bam_reader.rs
+++ src/bam_reader.rs
@@ -66,19 +66,25 @@ impl BamIndexedReader {
     }
 
     fn query(&mut self, region: &str) -> PyResult<PyObject> {
-        let mut config = new_exon_config();
-        if let Some(batch_size) = self.batch_size {
-            config = config.with_batch_size(batch_size);
-        }
-
-        let ctx = SessionContext::with_config_exon(config);
+        let ctx = SessionContext::new_exon();
 
         let df = self._runtime.block_on(async {
-            match ctx.query_bam_file(self.path.as_str(), region).await {
+            ctx.sql(&format!(
+                "CREATE EXTERNAL TABLE bam_file STORED AS INDEXED_BAM LOCATION '{}'",
+                self.path.as_str()
+            ))
+            .await?;
+
+            let sql = format!(
+                "SELECT * FROM bam_file WHERE bam_region_filter('{}', reference) = true",
+                region
+            );
+
+            match ctx.sql(&sql).await {
                 Ok(df) => Ok(df),
                 Err(e) => Err(io::Error::new(
                     io::ErrorKind::Other,
-                    format!("Error reading GFF file: {e}"),
+                    format!("Error reading BAM file: {e}"),
                 )),
             }
         })?;
