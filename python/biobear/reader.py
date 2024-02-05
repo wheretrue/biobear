ddef2febd6266096fa2d09be493793b25ecc290b
Trent Hauck <trent@trenthauck.com>
2024-02-05 11:27:15 -0800

    revert: revert genbank update

--- python/biobear/reader.py
+++ python/biobear/reader.py
@@ -60,10 +60,8 @@ class Reader(ABC):
                 "The 'polars' package is required to use the to_polars method."
             ) from import_error
 
-        arrow_record = self.to_arrow()
-        table = pa.Table.from_batches(arrow_record, schema=arrow_record.schema)
-
-        return pl.from_arrow(table)
+        pydict = self.to_arrow_scanner().to_table().to_pydict()
+        return pl.from_dict(pydict)
 
     def to_arrow_scanner(self) -> ds.Scanner:
         """Convert the inner data to an Arrow scanner.
