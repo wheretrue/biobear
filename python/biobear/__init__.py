76b6ce9853f0e862b67fe3bea2dcfac1ed5023c7
Trent Hauck <trent@trenthauck.com>
2024-03-20 19:12:33 -0700

    bump: version 0.17.14 → 0.17.15

--- python/biobear/__init__.py
+++ python/biobear/__init__.py
@@ -34,7 +34,7 @@ from .biobear import FileCompressionType
 from .biobear import FASTQReadOptions
 from .biobear import FASTAReadOptions
 
-__version__ = "0.17.14"
+__version__ = "0.17.15"
 
 __all__ = [
     "FastaReader",
