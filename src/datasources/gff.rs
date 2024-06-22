26df013331d7dc1b33c83d5ae93555fc38dc4352
Trent Hauck <trent@trenthauck.com>
2024-06-21 22:22:08 -0700

    feat: add gff file extension and compression inference (#155)

--- src/datasources/gff.rs
+++ src/datasources/gff.rs
@@ -16,46 +16,65 @@ use exon::datasources::gff::table_provider::ListingGFFTableOptions;
 use noodles::core::Region;
 use pyo3::{pyclass, pymethods, PyResult};
 
-use crate::FileCompressionType;
+use crate::{error::BioBearResult, file_options::FileOptions, FileCompressionType};
 
 use super::parse_region;
 
 #[pyclass]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct GFFReadOptions {
     region: Option<Region>,
-    file_compression_type: FileCompressionType,
-}
-
-impl Default for GFFReadOptions {
-    fn default() -> Self {
-        Self {
-            region: None,
-            file_compression_type: FileCompressionType::UNCOMPRESSED,
-        }
-    }
+    file_extension: Option<String>,
+    file_compression_type: Option<FileCompressionType>,
 }
 
 #[pymethods]
 impl GFFReadOptions {
     #[new]
-    #[pyo3(signature = (/, region = None, file_compression_type = None))]
+    #[pyo3(signature = (/, region = None, file_compression_type = None, file_extension=None))]
     fn try_new(
         region: Option<String>,
         file_compression_type: Option<FileCompressionType>,
+        file_extension: Option<String>,
     ) -> PyResult<Self> {
         let region = parse_region(region)?;
         Ok(Self {
             region,
-            file_compression_type: file_compression_type
-                .unwrap_or(FileCompressionType::UNCOMPRESSED),
+            file_compression_type,
+            file_extension,
         })
+    }
+}
+
+impl GFFReadOptions {
+    pub(crate) fn update_from_file_options(&mut self, options: &FileOptions) -> BioBearResult<()> {
+        if let Some(file_extension) = options.file_extension() {
+            if self.file_extension.is_none() {
+                self.file_extension = Some(file_extension.to_string());
+            }
+        }
+
+        if let Some(file_compression_type) = options.file_compression_type() {
+            if self.file_compression_type.is_none() {
+                let fct = FileCompressionType::try_from(file_compression_type)?;
+                self.file_compression_type = Some(fct);
+            }
+        }
+
+        Ok(())
     }
 }
 
 impl From<GFFReadOptions> for ListingGFFTableOptions {
     fn from(options: GFFReadOptions) -> Self {
-        let mut o = ListingGFFTableOptions::new(options.file_compression_type.into());
+        let file_compression_type = options
+            .file_compression_type
+            .unwrap_or(FileCompressionType::UNCOMPRESSED);
+
+        let file_extension = options.file_extension.unwrap_or("gff".to_string());
+
+        let mut o = ListingGFFTableOptions::new(file_compression_type.into())
+            .with_file_extension(Some(file_extension));
 
         if let Some(region) = options.region {
             o = o.with_region(region);
